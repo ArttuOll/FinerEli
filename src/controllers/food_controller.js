b25706f57d792b1486033552c50b7de6c9edb053
ArttuOll <arttuoll.tech@tutanota.com>
2020-12-16 10:55:21 +0200

    Lisää hakuparametrien validointi

--- src/controllers/food_controller.js
+++ src/controllers/food_controller.js
@@ -4,18 +4,21 @@ const sqlOperations = require("..util/sqlOperations");
 async function searchFoods(request, response) {
   const searchFoodDbOperation = sqlOperations.getDbOperation(
     searchFood,
-    handleError
+    handleServerSideError
   );
   searchFoodDbOperation(request, response);
 }
 
 async function searchFood(request, response) {
   const query = request.query;
-  const result = await sql.selectLike(query);
-  response.json({ statusCode: 200, result: result });
+  if (query) {
+    const result = await sql.selectLike(query);
+    response.json({ statusCode: 200, result: result });
+  }
+  response.json({ statusCode: 400, message: "Search query cannot be empty!" });
 }
 
-function handleError(error, response) {
+function handleServerSideError(error, response) {
   console.log("Error searching food from db: ", error.message);
   response.json({ statusCode: 500, message: "Error searching food from db" });
 }
