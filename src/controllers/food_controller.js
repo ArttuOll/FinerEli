527d6ebc93ada606ac5522697513ab119d54bdd7
ArttuOll <arttuoll.tech@tutanota.com>
2020-12-18 11:03:29 +0200

    Lisää lokitus ohjaimeen

--- src/controllers/food_controller.js
+++ src/controllers/food_controller.js
@@ -1,5 +1,6 @@
 const sql = require("../db/food_sql");
 const sqlOperations = require("../util/db_utils");
+const logger = require("../util/logger");
 
 async function searchFoods(request, response) {
   const searchFoodDbOperation = sqlOperations.getDbOperation(
@@ -11,10 +12,12 @@ async function searchFoods(request, response) {
 
 async function queryFoods(request, response) {
   const queryParameter = request.query.q;
+  logger.debug(`controllers.food_controller.queryFoods: Searching with query: ${queryParameter}`);
   if (queryParameter) {
     const result = await sql.selectLike(queryParameter);
     response.status(200).send({ result: result });
   } else {
+    logger.info("controllers.food_controller.queryFoods: Refusing to perform empty query");
     response.status(400).send({ message: "Search query cannot be empty!" });
   }
 }
@@ -28,13 +31,14 @@ async function getFoodComponents(request, response) {
 }
 
 async function queryComponents(request, response) {
-  const parameters = request.params.id;
-  const result = await sql.selectFoodComponents(parameters);
+  const foodId = request.params.id;
+  logger.debug(`controllers.food_controller.queryFoods: Searching components for id ${foodId}`);
+  const result = await sql.selectFoodComponents(foodId);
   response.status(200).send({ result: result });
 }
 
 function handleServerSideError(error, response) {
-  console.log("Server-side error", error.message);
+  logger.error(`controllers.food_controller.handleServerSide: Error searching db: ${error}`);
   response.status(500).send({ message: "Server-side error" });
 }
 
