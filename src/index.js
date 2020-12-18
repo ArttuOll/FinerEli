b9f9a79bdf381ab58d90c2bd7ed0dd8251c6b70d
ArttuOll <arttuoll.tech@tutanota.com>
2020-12-18 14:40:02 +0200

    Lisää kutsurajoitin

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const foodRoutes = require("./routes/food_routes");
 const logger = require("./util/logger");
+const rateLimit = require("express-rate-limit");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
@@ -16,12 +17,17 @@ const cors = (request, response, next) => {
   response.header("Acces-Control-Allow-Headers", "Content-Type");
   next();
 };
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 10
+});
 
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors);
 app.use(morgan("combined", { stream: logger.stream }));
+app.use(limiter);
 app.use(foodRoutes);
 
 app.listen(port, host, () => {
