dfec8ebacea1234028013a8ad7e3b147fe836c94
ArttuOll <arttuoll.tech@tutanota.com>
2020-12-18 11:02:57 +0200

    Luo lokittaja

---
+++ src/util/logger.js
@@ -0,0 +1,37 @@
+const { createLogger, format, transports } = require("winston");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const timestampFormat = { format: "YYYY-MM-DD HH:mm:ss" }
+const logFormat = format.combine(format.timestamp(timestampFormat), format.json());
+
+const logger = createLogger({
+  level: "debug",
+  transports: [
+    new transports.File({
+      filename: "./logs/errors.log",
+      level: "error",
+      format: logFormat
+    }),
+    new transports.File({
+      filename: "./logs/combined.log",
+      level: "info",
+      format: logFormat
+    })
+  ]
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(new transports.Console({
+    level: "debug",
+    format: format.combine(
+      format.timestamp(timestampFormat),
+      format.colorize(),
+      format.simple(),
+      format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    )
+  }));
+}
+
+module.exports = logger;
