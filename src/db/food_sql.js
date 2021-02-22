f87d3e831e8f53499295c7c6471cae2d3af3d8a4
ArttuOll <arttuoll.tech@tutanota.com>
2021-02-22 15:47:48 +0200

    Muuta komponenttihaun sql-lausetta
    Hakulauseeseen lisätty komponentin luokka ja järjestetty sen
    perusteella. Tämä tekee tiedon käsittelystä komponenttiluokittain asiakasohjelmistossa
    huomattavasti helpompaa.

--- src/db/food_sql.js
+++ src/db/food_sql.js
@@ -8,12 +8,12 @@ if (process.env.NODE_ENV !== "production") {
  * Koko ohjelman käyttämä tietokantayhteys. Tietokannan on oltava
  * MySQL/MariaDB-tietokanta.
  * @exports
-*/
+ */
 const connection = mysql.createConnection({
   host: process.env.HOSTNAME,
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
 });
 
 /*
@@ -21,7 +21,7 @@ const connection = mysql.createConnection({
  * voi esiintyä missä tahansa kohtaa ruoan nimeä. Tulokset palautetaan ruokien
  * id:n mukaan järjestettynä.
  * @exports
-*/
+ */
 function selectLike(queryParameter) {
   const query = `
                 SELECT f.FOODID AS food_id,
@@ -36,23 +36,24 @@ function selectLike(queryParameter) {
 
 /*
  * Hakee tietokannasta ruoan komponentit ruoan id:n perusteella. Eri
- * komponentteja on yhteensä 74 ja ne on järjestetty komponentin koko nimen
+ * komponentteja on yhteensä 74 ja ne on järjestetty komponentin luokan
  * perusteella.
  * @exports
-*/
+ */
 function selectFoodComponents(foodId) {
   const query = `
                 SELECT cv.FOODID AS foodid,
                         cn.DESCRIPT AS description,
                         cv.BESTLOC AS value,
                         c2.compunit AS unit_abbrev,
+                        c2.CMPCLASSP AS component_class,
                         c3.DESCRIPT AS unit
                 FROM component_value cv
                 JOIN component_names cn ON cn.THSCODE = cv.EUFDNAME
                 JOIN component c2 ON c2.EUFDNAME = cv.EUFDNAME
                 JOIN compunit c3 ON c3.THSCODE = c2.COMPUNIT
                 WHERE cv.FOODID = ?
-                ORDER BY cn.DESCRIPT;
+                ORDER BY c2.CMPCLASSP;
                  `;
   return dbUtils.executeSql(connection, query, [foodId]);
 }
@@ -60,5 +61,5 @@ function selectFoodComponents(foodId) {
 module.exports = {
   selectLike: selectLike,
   selectFoodComponents: selectFoodComponents,
-  connection: connection
-}
+  connection: connection,
+};
